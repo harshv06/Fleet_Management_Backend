2d164d90b1b5fedb0da4e2221ce1ad32fa2c3ed4
Jaydeepwakade <jaydeepwakade123@gmail.com>
2025-03-05 21:15:09 +0530

    login and registration fix

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -3,6 +3,7 @@ const AuthController = require("../controllers/Authentication/authController");
 const { validateToken, refreshToken } = require("../middlewares/authMiddleware");
 
 // router.post("/register", AuthController.register);
+router.post("/signup", AuthController.signup)
 router.post("/login", AuthController.login);
 router.post("/validate-token", validateToken, AuthController.validateToken);
 router.post("/refresh-token", refreshToken);
